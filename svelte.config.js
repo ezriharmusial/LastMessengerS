bf79dd80044ef3aff5046b6bb1d505e80dc21c16
ezriharmusial <ezriharmusial@gmail.com>
2022-04-18 23:19:23 +0200

    Firebase & Msvdex

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ fallback: './index.html' })
 	}
 };
 
