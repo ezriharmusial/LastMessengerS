79cbd0330a3f0cf62d442c25e0747ba29c63f515
ezriharmusial <ezriharmusial@gmail.com>
2022-09-29 23:59:07 +0200

    Transform to LastMessengerS.TV repository

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,4 +1,7 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+import relativeImages from 'mdsvex-relative-images'
+import { visit } from 'unist-util-visit'
+
 
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
@@ -8,11 +11,56 @@ const config = defineConfig({
 	},
     "layout": {
 		_: "./src/layouts/default.svelte",
-		artist: "./src/layouts/artist.svelte",
-		artists: "./src/layouts/artists.svelte"
 	},
-	remarkPlugins: [],
+	remarkPlugins: [audios, videos, relativeImages],
 	rehypePlugins: []
 });
 
 export default config;
+
+/**
+ * Adds support to video files in markdown image links
+ */
+ function audios() {
+	const extensions = ['mp3', 'wav']
+	return function transformer(tree) {
+	  visit(tree, 'image', (node) => {
+		if (extensions.some((ext) => node.url.endsWith(ext))) {
+		  node.type = 'html'
+		  node.value = `
+			  <audio
+				src="${node.url}"
+				autoplay
+				playsinline
+				loop
+				title="${node.alt}"
+			  />
+			`
+		}
+	  })
+	}
+}
+
+/**
+ * Adds support to video files in markdown image links
+ */
+ function videos() {
+	const extensions = ['mp4', 'webm']
+	return function transformer(tree) {
+	  visit(tree, 'image', (node) => {
+		if (extensions.some((ext) => node.url.endsWith(ext))) {
+		  node.type = 'html'
+		  node.value = `
+			  <video
+				src="${node.url}"
+				autoplay
+				muted
+				playsinline
+				loop
+				title="${node.alt}"
+			  />
+			`
+		}
+	  })
+	}
+  }
--- svelte.config.js
+++ svelte.config.js
@@ -1,33 +1,25 @@
-import adapter from '@sveltejs/adapter-static';
-import { imagetools } from 'vite-imagetools';
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config.js';
+import { mdsvex } from 'mdsvex'
+import autoprefixer from 'autoprefixer'
+import mdsvexConfig from './mdsvex.config.js'
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
-	preprocess: [
-		preprocess({
-			scss: {
-				prependData: '@use "src/variables.scss" as *;'
-			}
-		}),
-		mdsvex(mdsvexConfig)
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess:[
+		mdsvex(mdsvexConfig),
+		preprocess()
 	],
-
 	kit: {
-		adapter: adapter({ fallback: './index.html' }),
-		vite: {
-			plugins: [imagetools({ force: true })],
+		adapter: adapter(),
 
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: '@use "src/variables.scss" as *;'
-					}
-				}
-			}
+
+    	// remove this if you don't want prerendering
+		prerender: {
+			entries: ['*', '/sitemap.xml', '/rss.xml']
 		}
 	}
 };
