cd331bca357a4efc60a56c096810544187c29b8c
ezriharmusial <ezriharmusial@gmail.com>
2022-09-30 22:19:37 +0200

    config fix

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,7 +1,4 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
-import relativeImages from 'mdsvex-relative-images'
-import { visit } from 'unist-util-visit'
-
 
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
@@ -9,58 +6,9 @@ const config = defineConfig({
 	smartypants: {
 		dashes: 'oldschool'
 	},
-    "layout": {
-		_: "./src/layouts/default.svelte",
-	},
-	remarkPlugins: [audios, videos, relativeImages],
+
+	remarkPlugins: [],
 	rehypePlugins: []
 });
 
 export default config;
-
-/**
- * Adds support to video files in markdown image links
- */
- function audios() {
-	const extensions = ['mp3', 'wav']
-	return function transformer(tree) {
-	  visit(tree, 'image', (node) => {
-		if (extensions.some((ext) => node.url.endsWith(ext))) {
-		  node.type = 'html'
-		  node.value = `
-			  <audio
-				src="${node.url}"
-				autoplay
-				playsinline
-				loop
-				title="${node.alt}"
-			  />
-			`
-		}
-	  })
-	}
-}
-
-/**
- * Adds support to video files in markdown image links
- */
- function videos() {
-	const extensions = ['mp4', 'webm']
-	return function transformer(tree) {
-	  visit(tree, 'image', (node) => {
-		if (extensions.some((ext) => node.url.endsWith(ext))) {
-		  node.type = 'html'
-		  node.value = `
-			  <video
-				src="${node.url}"
-				autoplay
-				muted
-				playsinline
-				loop
-				title="${node.alt}"
-			  />
-			`
-		}
-	  })
-	}
-  }
