fa89833cb1b9f61343538fc2b0c3146208a5f57c
ezriharmusial <ezriharmusial@gmail.com>
2022-09-30 23:00:46 +0200

    build error

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase-admin'
+import admin from 'firebase-admin'
 import { getAuth } from 'firebase-admin/auth'
 
 const useEmulator = true
@@ -7,6 +7,15 @@ if (useEmulator) {
   process.env['FIREBASE_AUTH_EMULATOR_HOST'] = '127.0.0.1:9099'
 }
 
+function initializeFirebase() {
+	if (!admin.apps.length) {
+		return admin.initializeApp({ projectId: 'last-messengers' });
+	} else {
+    return admin.apps[0]
+  }
+}
+
+
 // this is the server-side firebase client
-export const app = initializeApp({ projectId: 'last-messengers' }, 'last-messengers-admin')
+export const app = initializeFirebase()
 export const auth = getAuth(app)
