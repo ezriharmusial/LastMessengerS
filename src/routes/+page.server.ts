8e1310e55008dc382dbd952ab9392e11194c7535
ezriharmusial <ezriharmusial@gmail.com>
2022-10-01 15:26:22 +0200

    cons

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import { fetchMediaMD } from '$lib/md-collections/fetchMedia'
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-  console.log('+page.server.ts -> media', fetchMediaMD)
+
   return {
     // eslint-disable-next-line no-unused-vars
     // recentMedia: media.slice(0, 2)
