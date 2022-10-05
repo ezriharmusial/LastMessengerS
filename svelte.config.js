0819454f4bba01ea8d7137450859ef64e2015e6f
ezriharmusial <ezriharmusial@gmail.com>
2022-10-05 21:20:21 +0200

    Vercel Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import { mdsvex } from 'mdsvex'
-import autoprefixer from 'autoprefixer'
+// import autoprefixer from 'autoprefixer'
 import mdsvexConfig from './mdsvex.config.js'
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +15,20 @@ const config = {
 		preprocess()
 	],
 	kit: {
-		adapter: adapter(),
+		adapter: vercel({
+			// if true, will deploy the app using edge functions
+			// (https://vercel.com/docs/concepts/functions/edge-functions)
+			// rather than serverless functions
+			edge: false,
+
+			// an array of dependencies that esbuild should treat
+			// as external when bundling functions
+			external: [],
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		  }),
 
 
     	// remove this if you don't want prerendering
