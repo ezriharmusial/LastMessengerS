36f9a2393ae30ebac095f1f712a413bdfb1accee
ezriharmusial <ezriharmusial@gmail.com>
2022-10-08 23:21:00 +0200

    Directly into server.ts

--- src/routes/api/admin/auth/+server.ts
+++ src/routes/api/admin/auth/+server.ts
@@ -1,27 +1,53 @@
 import { error, redirect } from "@sveltejs/kit";
 import { AuthorizationCode } from "simple-oauth2";
 import { randomBytes } from "crypto";
-import { config } from "../config";
 import { scopes } from "../scopes";
 
 export const randomString = () => randomBytes(4).toString("hex");
 
 /** @type {import('./$types').PageServerLoad} */
 export async function GET({ params, url}){
+
+	const config = provider => ({
+		client: {
+			id: client[provider].id,
+			secret: client[provider].secret
+		},
+		auth: {
+			tokenHost: auth[provider].tokenHost,
+			tokenPath: auth[provider].tokenPath,
+			authorizePath: auth[provider].authorizePath
+		}
+	});
+
+	const auth = {
+		github: {
+			tokenHost: "https://github.com",
+			tokenPath: "/login/oauth/access_token",
+			authorizePath: "/login/oauth/authorize"
+		},
+	};
+
+	const client = {
+		github: {
+			id: import.meta.env.PRIVATE_OAUTH_GITHUB_CLIENT_ID,
+			secret: import.meta.env.PRIVATE_OAUTH_GITHUB_CLIENT_SECRET
+		}
+	};
+
 	// If there are no Env. variables are unconfigured, or you are not live on Vercel
 	// if (config?.auth?.tokenHost == undefined)
 	// throw error(418, 'You are a Teapot!');
 
-	const provider = params.provider;
 
 	// simple-oauth will use our config files to generate a client we can use to request access
-	const client = new AuthorizationCode(config(provider));
+	const authClient = new AuthorizationCode(config(params.provider));
 
 	// we then make a build the request to our provider
-	const authorizationUri = client.authorizeURL({
+	const authorizationUri = authClient.authorizeURL({
 		// your callback url is important! More on this later
-		redirect_uri: `https://${url.host}/api/admin/callback?provider=${provider}`,
-		scope: scopes[provider],
+		redirect_uri: `https://${url.host}/api/admin/callback?provider=${params.provider}`,
+		scope: scopes[params.provider],
 		state: randomString()
 	});
 
