26631dbd85e941bead0a913307900d692b2c61c4
ezriharmusial <ezriharmusial@gmail.com>
2022-10-15 22:56:09 +0200

    netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,8 @@ import { mdsvex } from 'mdsvex'
 // import autoprefixer from 'autoprefixer'
 import mdsvexConfig from './mdsvex.config.js'
 // import adapter from '@sveltejs/adapter-auto';
-import vercel from '@sveltejs/adapter-vercel';
+// import vercel from '@sveltejs/adapter-vercel';
+import netlify from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -17,20 +18,7 @@ const config = {
         })
 	],
 	kit: {
-		adapter: vercel({
-			// if true, will deploy the app using edge functions
-			// (https://vercel.com/docs/concepts/functions/edge-functions)
-			// rather than serverless functions
-			edge: false,
-
-			// an array of dependencies that esbuild should treat
-			// as external when bundling functions
-			external: [],
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false
-		  }),
+		adapter: netlify(),
 
 
     	// remove this if you don't want prerendering
