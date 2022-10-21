a260a0c6e8e371e4c51c4853e2302f1c41f25fa4
ezriharmusial <ezriharmusial@gmail.com>
2022-10-21 20:37:48 +0200

    Fixed Nav styles.

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -14,12 +14,12 @@
 {/if}
 
 
-<div class="offCanvas fixed top-0 left-0 w-60 h-full bg-black flex flex-col justify-start text-white pt-20" class:visible={$UI.menu.visible}>
-	<main class="flex-grow-1 flex-shrink-1 overflow-y-auto m-auto">
+<div class="offCanvas fixed top-0 left-0 transition-transform duration-700 {$UI.menu.visible ? 'translate-x-0' : '-translate-x-full'} sm:w-3/4 lg:w-80 h-full bg-black flex flex-row-reverse portrait:flex-col landscape:sm:flex-row-reverse landscape:lg:flex-col sm:justify-around lg:justify-start text-white portrait:pt-20 landscape:md:pt-20">
+	<main class="h-full lg:flex-grow-1 lg:flex-shrink-1 overflow-y-auto">
 		<Tracks />
 	</main>
 
-	<footer class="flex-grow-0 h-60 flex-shrink-0 bg-white">
+	<footer class="flex-grow-0 w-60 lg:w-80 flex-shrink-0 m-auto">
 		{#if $player.track}
 		<img class="object-cover v-full h-full bg-gradient-to-br from-slate-900 to-black" data-amplitude-song-info="cover_art_url" src="{$player.track.image || getArtistImage($player.track.artist)}" alt="Track CoverArt"/>
 		{/if}
@@ -27,14 +27,3 @@
 </div>
 
 <Login />
-
-<style lang="scss">
-	.offCanvas {
-		transform: translate(-15rem);
-		transition: transform 0.5s;
-	}
-
-	.offCanvas.visible {
-		transform: translate(0);
-	}
-</style>
--- src/lib/components/TrackMediaObject.svelte
+++ src/lib/components/TrackMediaObject.svelte
@@ -8,29 +8,29 @@
 
 {#if medium}
 <div class="notification bg-black hover:bg-slate-900 text-white blurred has-shadow has-pointer-cursor py-2 px-1">
-    <article class="media flex items-center" in:fade>
+    <article class="media flex items-center sm:text-md lg:text-2xl" in:fade>
         {#if medium.order == $player.track.order}
-        <figure class="w-4 h-4 mx-2">
-            <svg id="play-icon px-2" width="10" height="14" viewBox="0 0 31 37" fill="currentColor" xmlns="http://www.w3.org/2000/svg" transition:fade>
+        <figure class="w-4 h-4 mx-2 lg:w-5 lg:h-5 lg:mx-4">
+            <svg id="play-icon px-2 w-3 h-3 lg:w-5 lg:h-5 lg:mx-4" viewBox="0 0 31 37" fill="currentColor" xmlns="http://www.w3.org/2000/svg" transition:fade>
                 <path clip-rule="evenodd" d="M29.6901 16.6608L4.00209 0.747111C2.12875 -0.476923 0.599998 0.421814 0.599998 2.75545V33.643C0.599998 35.9728 2.12747 36.8805 4.00209 35.6514L29.6901 19.7402C29.6901 19.7402 30.6043 19.0973 30.6043 18.2012C30.6043 17.3024 29.6901 16.6608 29.6901 16.6608Z" class="fill-black dark:fill-white"/>
     		</svg>
         </figure>
         {:else}
-        <p class="text-md w-4 h-4 mx-2">
+        <p class="w-4 h-4 mx-2 lg:w-6 lg:h-6 lg:mx-4">
             {(medium.order < 10) ? "0" + medium.order : medium.order}
         </p>
         {/if}
-        <figure class="relative h-7 w-7 mr-2">
-            <img class="object-cover h-7 w-7 bg-gradient-to-br from-slate-900 to-black" alt="Track Cover" src="{medium.image || getArtistImage(medium.artist) || '/uploads/albumcovers/happysunofyah.jpg'}" width="64" height="64">
+        <figure class="relative h-7 w-7 mr-2 lg:w-8 lg:h-8 lg:mr-4">
+            <img class="object-cover h-7 w-7 lg:w-8 lg:h-8 bg-gradient-to-br from-slate-900 to-black" alt="Track Cover" src="{medium.image || getArtistImage(medium.artist) || '/uploads/albumcovers/happysunofyah.jpg'}" width="64" height="64">
             <span class="play icon small">
                 <i class="fas fa-play"></i>
             </span>
         </figure>
         <div>
-            <p class="title text-md text-bold w-max truncate overflow-hidden">
+            <p class="title font-semibold w-max truncate overflow-hidden">
                 {medium.title}
             </p>
-            <p class="subtitle text-md w-max truncate overflow-hidden">
+            <p class="subtitle w-max truncate overflow-hidden">
                 by {medium.artist}
             </p>
         </div>
