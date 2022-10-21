503d566a267c00907d5f01724d57b2f4e7798890
ezriharmusial <ezriharmusial@gmail.com>
2022-10-21 18:58:21 +0200

    Nav is mobile

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -14,12 +14,12 @@
 {/if}
 
 
-<div class="offCanvas bg-black text-white pt-20" class:visible={$UI.menu.visible}>
-	<main class="flex-grow-1 flex-shrink-1 overflow-y-auto">
+<div class="offCanvas fixed top-0 left-0 w-60 h-full bg-black flex flex-col justify-start text-white pt-20" class:visible={$UI.menu.visible}>
+	<main class="flex-grow-1 flex-shrink-1 overflow-y-auto m-auto">
 		<Tracks />
 	</main>
 
-	<footer class="flex-grow-0 flex-shrink-0 bg-white">
+	<footer class="flex-grow-0 h-60 flex-shrink-0 bg-white">
 		{#if $player.track}
 		<img class="object-cover v-full h-full bg-gradient-to-br from-slate-900 to-black" data-amplitude-song-info="cover_art_url" src="{$player.track.image || getArtistImage($player.track.artist)}" alt="Track CoverArt"/>
 		{/if}
@@ -30,27 +30,11 @@
 
 <style lang="scss">
 	.offCanvas {
-		position: fixed;
-		top: 0;
-		left: 0;
-		height: 100vh;
-		width: 15rem;
 		transform: translate(-15rem);
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-start;
 		transition: transform 0.5s;
-	}
-
-	footer {
-		height: 15rem;
 	}
 
 	.offCanvas.visible {
 		transform: translate(0);
-	}
-
-	main {
-		margin-bottom: auto;
 	}
 </style>
