bdbef85ea19cbcdd58454a451efd03f5a47aaf8e
ezriharmusial <ezriharmusial@gmail.com>
2022-10-22 17:23:36 +0200

    Static

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 import { mdsvex } from 'mdsvex'
 // import autoprefixer from 'autoprefixer'
 import mdsvexConfig from './mdsvex.config.js'
-import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 // import adapter from '@sveltejs/adapter-vercel';
 // import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
@@ -20,14 +20,10 @@ const config = {
 	],
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html'
-		}),
-
-
-		// remove this if you don't want prerendering
-		prerender: {
-			// TODO: Find repo with these files// entries: ['*', '/sitemap.xml', '/rss.xml']
-		}
+                pages: 'build',
+                assets: 'build',
+                fallback: 'index.html'
+            }),
 	}
 };
 
