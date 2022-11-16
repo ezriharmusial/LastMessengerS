042d4932e9b8354f27eea0155b7ddaa3f3b986ea
ezriharmusial <ezriharmusial@gmail.com>
2022-11-16 20:59:06 +0100

    browser

--- src/routes/artists/Artists.svelte
+++ src/routes/artists/Artists.svelte
@@ -4,6 +4,7 @@
 	import Artist from '$lib/components/Artist.svelte';
 
 	import lazyload from 'vanilla-lazyload';
+	import { browser } from '$app/environment';
 
 	if (browser && !document.lazyloadInstance) {
 		document.lazyloadInstance = new lazyload();
