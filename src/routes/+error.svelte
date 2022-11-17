6759d010e7d947ef37c42832ae70f3eba5c84fbc
ezriharmusial <ezriharmusial@gmail.com>
2022-11-17 14:13:26 +0100

    Error Page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,67 +1,8 @@
 <script>
 	import { page } from '$app/stores';
 
-	import lazyload from 'vanilla-lazyload';
+	import Footer from '$lib/components/Footer.svelte';
 
-	import SEO from "$lib/components/SEO/index.svelte";
-	import website from "$lib/config/website";
-
-	let ogSquareImageSrc = '/images/screenshot.png';
-	let ogImageSrc = '/images/screenshot.png';
-	let twitterImageSrc = '/images/screenshot.png';
-	let featuredImageSrc = '/images/screenshot.png';
-
-	const { author, siteUrl } = website;
-	let title = 'Home';
-	const breadcrumbs = [
-		{
-			name: 'Home',
-			slug: '',
-		},
-	];
-	let metadescription =
-		'LastMessengerS Music - connects talented underpriviledged musicians with Management & ICT Professionals to restore Truth in Music';
-	const featuredImageAlt =
-		'picture of a person with long, curly hair, wearing a red had taking a picture with an analogue camera';
-	const featuredImage = {
-		url: siteUrl + featuredImageSrc,
-		alt: featuredImageAlt,
-		width: 672,
-		height: 448,
-		caption: 'Home page',
-	};
-	const ogImage = {
-		url: siteUrl + ogImageSrc,
-		alt: featuredImageAlt,
-	};
-	const ogSquareImage = {
-		url: siteUrl + ogSquareImageSrc,
-		alt: featuredImageAlt,
-	};
-
-	const twitterImage = {
-		url: siteUrl + twitterImageSrc,
-		alt: featuredImageAlt,
-	};
-	const entityMeta = {
-		url: `${siteUrl}/`,
-		faviconWidth: 512,
-		faviconHeight: 512,
-		caption: author,
-	};
-	const seoProps = {
-		title,
-		slug: '',
-		entityMeta,
-		datePublished: '2021-07-07T14:19:33.000+0100',
-		lastUpdated: '2021-07-07T14:19:33.000+0100',
-		breadcrumbs,
-		metadescription,
-		featuredImage,
-		ogImage,
-		ogSquareImage,
-		twitterImage,
-	};
 	const dev = process.env.NODE_ENV === 'development';
 </script>
 
@@ -87,24 +28,25 @@
 	}
 </style>
 
-<!-- <svelte:head>
+<svelte:head>
 	{#if $page}
 	<title>{$page.status}</title>
 	{/if}
-</svelte:head> -->
-
-<svelte:head>
-	<SEO {...seoProps} />
 </svelte:head>
+
+<main class="pt-32 px-10 content text-3xl">
 
 {#if $page}
 
-<h1>{$page.status}</h1>
-
-<p>{$page.error.message}</p>
+<h1 class="text-4xl marker">{$page.status}</h1>
+{#if $page.error.message}
+<p>Sorry we couldn't find what you are finding because of a <em>{$page.error.message}</em></p>
+{/if}
 
 {#if dev && $page.error.stack}
 <pre>{$page.error.stack}</pre>
 {/if}
 
 {/if}
+</main>
+<Footer />
