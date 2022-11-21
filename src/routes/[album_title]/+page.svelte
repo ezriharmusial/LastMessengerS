8fa5e19d95135d500b9e75cebb56a8becefb58ff
ezriharmusial <ezriharmusial@gmail.com>
2022-11-21 13:53:27 +0100

    rise 'n' shine

--- src/routes/[album_title]/+page.svelte
+++ src/routes/[album_title]/+page.svelte
@@ -25,7 +25,7 @@
 		<div class="grid lg:grid-cols-2 gap-12 flex items-center">
 			<div class="mt-12 lg:mt-0">
 				<h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12" style="color: hsl(218, 81%, 95%);">Africa4Africa <br /><span style="color: hsl(218, 81%, 75%);">{ data.title }</span></h1>
-				<a class="inline-block px-7 py-3 mr-2 bg-gray-200 text-gray-700 font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="/unity-album/rise-n-shine" role="button">Play Album</a>
+				<a class="inline-block px-7 py-3 mr-2 bg-gray-200 text-gray-700 font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="/unity-album/rise-'n'-shine" role="button">Play Album</a>
 				<a class="inline-block px-7 py-3 bg-transparent text-white font-medium text-sm leading-snug uppercase rounded focus:outline-none focus:ring-0 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#more" role="button">Learn more</a>
 			</div>
 			<div class="mb-12 lg:mb-0">
